42fbcd890fcf16d6f468c7310c557a329eb64779
Hsu Pyae Pyae Aung <2403894E@student.tp.edu.sg>
2025-11-26 12:01:32 +0800

    Added Itentity

---
+++ CarRentalManagement/Migrations/20251124184251_initial.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Booking",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VehicleId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Booking", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colour",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colour", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DrivingLicense = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Make",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Make", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    year = table.Column<int>(type: "int", nullable: false),
+                    LicensePlateNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MakeId = table.Column<int>(type: "int", nullable: false),
+                    ModelId = table.Column<int>(type: "int", nullable: false),
+                    ColourId = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Colour",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(214), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(227), "Black", "System" },
+                    { 2, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(230), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(231), "Blue", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Make",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(451), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(452), "BMW", "System" },
+                    { 2, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(454), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(455), "Toyota", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Model",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(543), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(544), "i4", "System" },
+                    { 2, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(546), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(547), "X5", "System" },
+                    { 3, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(549), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(550), "Prius", "System" },
+                    { 4, "System", new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(551), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(552), "C-HR", "System" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Booking");
+
+            migrationBuilder.DropTable(
+                name: "Colour");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Make");
+
+            migrationBuilder.DropTable(
+                name: "Model");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
---
+++ CarRentalManagement/Migrations/20251124184411_SeedData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8752), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8765) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8767), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8768) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8998), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(8999) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9001), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9002) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9086), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9087) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9089), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9089) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9091), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9092) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9094), new DateTime(2025, 11, 25, 2, 44, 10, 960, DateTimeKind.Local).AddTicks(9095) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(214), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(227) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(230), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(231) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(451), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(452) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(454), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(455) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(543), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(544) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(546), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(547) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(549), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(550) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(551), new DateTime(2025, 11, 25, 2, 42, 50, 681, DateTimeKind.Local).AddTicks(552) });
+        }
+    }
+}
